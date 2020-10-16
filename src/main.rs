995ab8acd89c192b67f4163c08b2d53539147a71
scrummer123 <simonpeters05@gmail.com>
2020-10-16 17:24:09 +0200

    :zap: Testing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::{App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("ezRest").get_matches();
+
+    match matches.subcommand() {
+        ("make", Some(_)) => println!("LOLLER"),
+        _ => println!("yeet")
+    }
 }
